92e86682c70c2db4c5cbb809248b8fbd89b9e842
kesavan75thaagam <kesavan@thaagam.email>
2026-01-10 22:10:55 +0530

    SimpleUserModel

--- MahalApp/models.py
+++ MahalApp/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+# models.py
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('manager', 'Manager'),
+        ('client', 'Client'),
+    )
+
+    phone = models.CharField(max_length=15, unique=True)
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default='client'
+    )
+    address = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.username
--- MarriageMahalProject/settings.py
+++ MarriageMahalProject/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL='MahalApp.User'
 
 # Application definition
 
