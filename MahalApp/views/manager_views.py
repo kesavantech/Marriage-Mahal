fe87bdd9e01ceef9e6758b670a3cc57776360e61
kesavan75thaagam <kesavan@thaagam.email>
2026-03-25 15:22:31 +0530

    Manager_Manage_Booking_Process

--- MahalApp/views/manager_views.py
+++ MahalApp/views/manager_views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.core.paginator import Paginator
 from MahalApp.models import Booking
 
 
 @login_required
 def manage_bookings_view(request):
-    if request.user.role != 'manager':
+    if request.user.role not in ['manager', 'admin']:
         messages.error(request, 'Access Denied!')
         return redirect('dashboard')
-    bookings = Booking.objects.all().order_by('-created_at')
+    all_bookings = Booking.objects.all().order_by('-created_at')
+    paginator = Paginator(all_bookings, 8)
+    page = request.GET.get('page', 1)
+    bookings = paginator.get_page(page)
     return render(request, 'manager/manage_bookings.html', {'bookings': bookings})
 
 
 @login_required
 def confirm_booking_view(request, booking_id):
-    if request.user.role != 'manager':
+    if request.user.role not in  ['manager','admin']:
         messages.error(request, 'Access Denied!')
         return redirect('dashboard')
     try:
@@ -25,7 +29,7 @@ def confirm_booking_view(request, booking_id):
             return redirect('manage_bookings')
         booking.status = 'Confirmed'
         booking.save()
-        messages.success(request, f'Booking #{booking_id} confirmed!')
+        messages.success(request, f'Booking #{booking.user.username} confirmed!')
     except Booking.DoesNotExist:
         messages.error(request, 'Booking not found!')
     return redirect('manage_bookings')
@@ -33,7 +37,7 @@ def confirm_booking_view(request, booking_id):
 
 @login_required
 def mark_advance_paid_view(request, booking_id):
-    if request.user.role != 'manager':
+    if request.user.role not in  ['manager','admin']:
         messages.error(request, 'Access Denied!')
         return redirect('dashboard')
     try:
@@ -48,7 +52,7 @@ def mark_advance_paid_view(request, booking_id):
 
 @login_required
 def reject_booking_view(request, booking_id):
-    if request.user.role != 'manager':
+    if request.user.role not in  ['manager','admin']:
         messages.error(request, 'Access Denied!')
         return redirect('dashboard')
     try:
